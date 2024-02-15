5a585f70a0126db4c8c83267e9dd6fb769efa3e9
Hudson H <Hudsonhargrove01@gmail.com>
2024-02-15 14:47:13 -0600

    added dafult characters

--- Character.py
+++ Character.py
@@ -17,24 +17,26 @@ class Character:
 
     def attack(self, target) -> None:
         while((target.alive==True) & (self.alive==True)):
+            x=0
+            while (x!=10):
+                if(target.health > 0):
 
-            if(target.health > 0):
+                    print(f"You: {self.health}/{self.maxHealth} Enemy: {target.health}/{target.maxHealth}")
 
-                print(f"You: {self.health}/{self.maxHealth} Enemy: {target.health}/{target.maxHealth}")
+                    target.health-=self.dmg
+                    print (f"{self.name} attacks {target.name} for {self.dmg} health")
 
-                target.health-=self.dmg
-                print (f"{self.name} attacks {target.name} for {self.dmg} health")
+                    self.health-=target.dmg
+                    print (f"{target.name} attacks {self.name} for {target.dmg} health")
 
-                self.health-=target.dmg
-                print (f"{target.name} attacks {self.name} for {target.dmg} health")
-
-                time.sleep(0.4)
-                os.system('cls')
-            else:
-                print(f"You: {self.health}/{self.maxHealth} Enemy: {target.health}/{target.maxHealth}")
-                target.alive = False
-                print(f"{target.name} was killed by {self.name}")
-                time.sleep(3)
+                    time.sleep(0.4)
+                    os.system('cls')
+                else:
+                    print(f"You: {self.health}/{self.maxHealth} Enemy: {target.health}/{target.maxHealth}")
+                    target.alive = False
+                    print(f"{target.name} was killed by {self.name}")
+                    time.sleep(3)
+                x+=1
 
     def equip(self,weapon):
         self.dmg=weapon.dmg
@@ -46,4 +48,5 @@ class Character:
             print(f"You: {self.health}/{self.maxHealth} Enemy: {target.health}/{target.maxHealth}")
 
 # Init of default Characters
-Hero = Character
+Hero = Character("Hero",100,100)
+Enemy = Character("Enemy",100,100)
